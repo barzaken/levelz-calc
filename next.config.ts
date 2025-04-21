6baeac00295ad643c310d9917b73ef38a48badc4
barzaken <bar.zaken@boyar.org.il>
2025-04-21 17:59:07 +0300

    after ignore-loader

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
